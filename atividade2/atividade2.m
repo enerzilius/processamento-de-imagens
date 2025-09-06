0b4384828463ed3f4bd27794afd2acacbf77065b
enerzilius <efblouback@gmail.com>
2025-09-06 00:23:40 -0300

    tudo funcionando

--- atividade2/atividade2.m
+++ atividade2/atividade2.m
@@ -1,14 +1,15 @@
 original = imread('pollen.jpg');
+figure, imshow(original);
 
 img_dimensions = size(original);
-matrix = zeros(img_dimensions(1), img_dimensions(2), 'uint8');
+new_image = zeros(img_dimensions(1), img_dimensions(2), 'uint8');
 
-function s = T (M, A, B, x)
-  if (x < A(1))
+function s = contrast_stretch (M, A, B, r)
+  if (r < A(1))
     m = M(1);
     s1 = 0;
     r1 = 0;
-  elseif (x >= A(1) && x < B(1))
+  elseif (r >= A(1) && r < B(1))
     m = M(2);
     s1 = A(2);
     r1 = A(1);
@@ -18,7 +19,7 @@ function s = T (M, A, B, x)
     r1 = B(1);
   endif
 
-  s = s1 + m * (x - r1);
+  s = s1 + m * (r - r1);
 endfunction
 
 # esses foram os pontos que eu achei que tiveram o resultado mais satisfatório
@@ -29,13 +30,13 @@ B = [125, 180]; # s2, r2
 m = (A(2) - 0) / (A(1) - 0);
 m1 = (B(2) - A(2)) / (B(1) - A(1));
 m2 = (255 - B(2)) / (255 - B(1));
-M = [m, m1, m2]
+M = [m, m1, m2];
 
 
 x = 1:256;
 y = zeros(1, 256);
 for i = 1:256
-  y(i) = T(M, A, B, x(i));
+  y(i) = contrast_stretch(M, A, B, x(i));
 endfor
 
 plot(x, y);
@@ -45,4 +46,10 @@ title("Gráfico da reta");
 grid on;
 axis([0, 255, 0, 255]);
 
+for i = 1:img_dimensions(1)
+  for j = 1:img_dimensions(2)
+    new_image(i, j) = contrast_stretch(M, A, B, original(i, j));
+  endfor
+endfor
+figure, imshow(new_image);
 
