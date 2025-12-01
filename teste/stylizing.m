1a974395395feae6d4e0e9f12208058228df2c8b
enerzilius <efblouback@gmail.com>
2025-11-30 21:34:38 -0300

    melhor

--- teste/stylizing.m
+++ teste/stylizing.m
@@ -2,7 +2,6 @@ pkg load image;
 
 reze = imread("reze.jpg");
 reze_double = im2double(reze);
-imshow(reze_double);
 
 sobel_x = [
   -1, 0, 1;
@@ -15,22 +14,26 @@ sobel_y = [
    1,  2,  1;
 ];
 
-m = 3;
+m = 13;
 
 gx = filter2(sobel_x, reze_double);
 gy = filter2(sobel_y, reze_double);
 M = abs(gx) + abs(gy);
+M = (1-reze_double) + M;
 M = M > 0.9;
+#M = M > 0.9;
+#M = 1.0 - M;
 
-figure, imshow(M);
+
+M = reze_double - M;
 
 gray_img = M;  % values in [0,1]
 
-tint = [0.8, 0.2, 0.7];  % RGB multipliers
+tint = [0.7, 0.0, 0.6];  % RGB multipliers
 
 colored = cat(3, gray_img * tint(1), gray_img * tint(2), gray_img * tint(3));
 
-imshow(colored);
-imwrite(colored, "reze_coloured.jpg");
+figure, imshow(colored);
+imwrite(colored, "org_s_M.jpg");
 
 
